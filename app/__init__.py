c6c6be9c950a5c69d570a5aaf2bd3036ceb25e7b
fr1t <frit.mail@mail.ru>
2021-08-09 03:21:11 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-socketio = SocketIO(logger=True, engineio_logger=True, async_mode='eventlet')
-
 def create_app(debug=False):
     """Create an application."""
     app = Flask(__name__)
@@ -12,6 +10,6 @@ def create_app(debug=False):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    socketio.init_app(app)
+    socketio = SocketIO(app, logger=True, engineio_logger=True)
     return app
 
